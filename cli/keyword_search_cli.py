c8f1a62cf908f181e2d0e9f81fb12d265f479883
Colin Juillard <colin.juillard@gmail.com>
2025-10-15 20:24:09 -0500

    Text preprocessing, tokenization, and partial matching

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 import json
-
+import string
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -19,18 +19,38 @@ def main() -> None:
     match args.command:
         case "search":
             # print the search query here
+            tokens = tokenize_string(sanitize_string(args.query))
             print(f"Searching for: {args.query}")
             for movie in movies:
-                if args.query in movie["title"]:
+                movie_tokens = tokenize_string(sanitize_string(movie["title"]))
+                if(check_partial_match(tokens, movie_tokens)):
                     matches.append(movie)
+                    continue
             
             for i, match in enumerate(matches):
-                print(f"{i+1}. {match["title"]}")
-
-            pass
+                print(f"{i+1}. {match['title']}")
         case _:
             parser.print_help()
 
+
+translation_table = str.maketrans("", "", string.punctuation)
+def sanitize_string(s: str) -> str:
+    s = s.lower()
+    s = s.translate(translation_table)
+    return s
+
+def tokenize_string(s: str) -> list[str]:
+    tokens = s.split()
+    return tokens
+    # tokens
+
+# Check if any search token is in any target token
+def check_partial_match(search_tokens: list[str], target_tokens: list[str]) -> bool:
+    for search_token in search_tokens:
+      for target_token in target_tokens:
+          if(search_token in target_token):
+              return True
+    return False
 
 if __name__ == "__main__":
     main()
